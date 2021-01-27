c4a1a36ec3b7cc761185cdfb727f1426459d8d90
ShawnDFox <mararmlav@gmail.com>
2021-01-27 18:29:23 -0500

    progreso en personaje

---
+++ Assets/Scripts/Character/BotEngine.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotEngine : MonoBehaviour
+{
+    
+    [SerializeField]
+    private int Velocidad_max;
+    public int Velocidad;
+
+    private BotHealth Health;
+    private void Awake()
+    {
+        Health = GetComponent<BotHealth>();
+        GetComponent<CharacterInputs>().OnMove += Move;
+        //GetComponent<CharacterInputs>().OnInteract += Interact;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Move(float Hor,float vert)
+    {
+        //
+        Debug.Log("Moving");
+        if (Hor != 0)
+        {
+            vert = 0;
+        }
+        Health.Carga -= 0.05f;
+
+        Vector3 movement = new Vector3(Hor, vert, 0);
+        transform.Translate(movement * Time.deltaTime * Velocidad);
+    }
+
+    
+
+   
+    /*private void OnTriggerStay2D(Collider2D collision)
+    {
+        var charger = collision.GetComponent<Charger>();
+        var Discharger = collision.GetComponent<DisCharger>();
+        if (charger != null)
+        {
+            Charge(charger.amount);
+        }
+        if (Discharger != null)
+        {
+            DisCharge(charger.amount);
+        }
+    }*/
+}
---
+++ Assets/Scripts/Character/BotHealth.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BotHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int Salud_max;
+    [SerializeField]
+    private int Carga_max;
+    [SerializeField]
+    private int Defensa_max;
+
+    public float Carga;
+    public int Salud;
+    public int Defensa;
+    
+    public bool Infrarojo;
+    
+
+    public event Action OnPlayerLose;
+    public event Action OnCharge;
+
+    private void OnEnable()
+    {
+        Salud = Salud_max;
+        Carga = Carga_max;
+        //OnplayerSlow += SlowCalc; subscripcion para metodos que alenticen al jugador
+    }
+
+    
+    private void TakeDamage(int damage)
+    {
+        Salud -= damage; 
+    }
+
+    private void Charge(int amount)
+    {
+        if (Carga <= Carga_max)
+            Carga += amount;
+    }
+
+    private void DisCharge(int amount)
+    {
+        Carga -= amount;
+    }
+
+    private void Update()
+    {
+        if (Carga <= 0 || Salud <= 0)
+        {
+            OnPlayerLose?.Invoke();
+        }
+
+    }
+
+
+}
