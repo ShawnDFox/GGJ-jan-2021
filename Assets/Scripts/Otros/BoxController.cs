3ce774f633b9cc5b180534b0fe1847e72f52d809
ShawnDFox <mararmlav@gmail.com>
2021-02-09 16:51:07 -0500

    Cambios a escena

---
+++ Assets/Scripts/Otros/BoxController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] BoxSprites;
+    private SpriteRenderer render;
+    [SerializeField]
+    private Vector2 startPosition;
+
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+        startPosition = transform.position;
+        ReDraw();
+    }
+
+
+    public void ReDraw()
+    {
+        render.sprite = BoxSprites[Random.Range(0, BoxSprites.Length + 1)];
+    }
+
+    public void Reposition()
+    {
+        transform.position = startPosition;
+    }
+}
